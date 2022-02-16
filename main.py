51448350683400f32ae8ea001d71de51c535741b
adilkhan_work_khc_pc <Azaidulla@khc.kz>
2022-02-16 16:58:29 +0600

    work_code_1:

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-# -*- coding: UTF-8 -*-
+﻿# -*- coding: UTF-8 -*-
 from fastapi import FastAPI, Form
 from starlette.requests import Request
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 from fastapi import FastAPI, Form, File, UploadFile
+from fastapi.responses import FileResponse
+import pandas as  pd
+from pathlib import Path
+import logging
+
+logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+
+## mypackage
+import functions
+from datetime import date
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -16,17 +26,22 @@ async def files(
                     username: str    = Form(...),
                     password: str    = Form(...),
                 ):
-    print('username', username)
-    print('password', password)
+    logging.info(f"username {str(username)}, pass {str(password)}")
+    
+    #pass
     if username == 'admin' and password == 'admin':
-
+        df = pd.read_csv('./datasets/loggs_k.csv')
+        df['отчетная дата'] = pd.to_datetime(df['отчетная дата'],format="%d.%m.%y")
+        last_report_date = df['отчетная дата'].max()
+        logging.info(f"last_report_date {str(last_report_date)[:10]}")
         return templates.TemplateResponse(
             'index.html',
             {
                 'request':  request,
-                'username': username,
+                'last_report_date': str(last_report_date)[:10],
             })
     else:
+        logging.info(f"Wrong pass username {username}, pass {password}")
         return('Wrong pass')
 
 @app.post("/file/")
@@ -34,12 +49,35 @@ async def upload_file(
                     request: Request, ## не понял зачем нужен
                     excel_file: UploadFile = File(...), ## наименование переменной должно быть одинаковым с наименованием  <input name="excel_file"
                 ):
-    return templates.TemplateResponse("accepted.html",
-            {
-                "request":      request,
-              #  "file_sizes":   len(excel_file),
-                "filenames":    excel_file.filename,
-             })
+    logging.info(f"uploading file {excel_file.filename}")
+    suffix = Path(excel_file.filename).suffix
+    if suffix !=".xlsx" and suffix !=".xls":
+        return {"info": f"file have not supported format = {suffix}"}
+    else:
+        file_location = f"./uploads/{excel_file.filename}"
+        with open(file_location, "wb+") as file_object:
+            file_object.write(excel_file.file.read())
+        try:
+            functions.add_merge(file_location)
+        except Exception as e:
+            logging.exception("Exception occurred")
+            logging.error(f"file '{excel_file.filename}' not supported")
+            return {"info": f"file '{excel_file.filename}' not supported"}
+        df = functions.load_data()
+        ids = functions.ids(df)
+        functions.vintage(df,ids).to_excel('./datasets/vintage/vintage_кик_' +str(date.today())+ '.xlsx')
+        logging.info(f"Обновление прошло успешно")
+        return {"info": f"Обновление прошло успешно"}
+
+@app.post("/download/")
+async def download_file(
+                    request: Request, ## не понял зачем нужен
+                    ):
+    ## временно статичным
+    logging.info("Скачивание винтажа")
+    file_location = functions.newest(path="./datasets/vintage")
+       
+    return FileResponse(file_location)
 
 @app.get("/")
 async def main(request: Request):
@@ -49,4 +87,4 @@ async def main(request: Request):
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run("main:app", host="192.168.5.68", port=8000, reload = True)
